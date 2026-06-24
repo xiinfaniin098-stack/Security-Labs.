c84d724baec7889e55df03eb120ad041ead2e3b6
guuxaaye <xiinfaniin098@gmail.com>
2026-06-24 17:36:53 +0300

    Refactor SQL queries for clarity and organization

--- sql_log_analysis.sql
+++ sql_log_analysis.sql
@@ -8,3 +8,21 @@ SELECT * FROM log_in_attempts WHERE event_id >= 100;
 
 Step 2:  Filter login attempts with an event ID between 100 and 150.
 SELECT * FROM log_in_attempts WHERE event_id BETWEEN 100 AND 150;
+
+
+1-- Retrieve after hours failed login attempts
+SELECT * FROM log_in_attempts WHERE login_time > '18:00' AND success = 0;
+
+2--Retrieve login attempts on specific dates
+SELECT * FROM log_in_attempts WHERE login_date = '2022-05-08' OR login_date = '2022-05-09';
+
+3-- Retrieve login attempts outside of Mexico
+SELECT * FROM log_in_attempts WHERE NOT country LIKE 'MEX%';
+4--Retrieve employees in Marketing
+ SELECT * FROM employees WHERE department = 'Marketing' AND office LIKE 'East%';
+
+5-- Retrieve employees in Finance or Sales
+SELECT * FROM employees WHERE department = 'Finance' OR department = 'Sales';
+
+--Retrieve all employees not in IT
+ SELECT * FROM employees WHERE NOT department = 'Information Technology';
