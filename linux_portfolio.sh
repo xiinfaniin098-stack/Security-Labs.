bd2ee419dddb69c21beb3ba8530068644f0da88d
guuxaaye <xiinfaniin098@gmail.com>
2026-07-04 03:16:13 +0300

    Create master linux portfolio script

---
+++ linux_portfolio.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Task 1. Ensure that APT is installed
+apt
+
+# Task 2. Install and uninstall the Suricata application
+sudo apt install  suricata 
+
+sudo apt remove suricata
+
+# Task 3. Install the tcpdump application
+sudo apt install tcpdump
+
+# Task 4. List the installed applications
+sudo apt list --installed
+
+# Task 5. Reinstall the Suricata application
+sudo apt install suricata
+apt list --installed
