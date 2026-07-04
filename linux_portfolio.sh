d21ed5426c79cc5d9195a1ee177845ab20ef78e7
guuxaaye <xiinfaniin098@gmail.com>
2026-07-04 17:15:42 +0300

    Remove SQL tasks from linux_portfolio.sh
    Removed SQL queries related to organization machines and employees.

--- linux_portfolio.sh
+++ linux_portfolio.sh
@@ -21,32 +21,3 @@ apt list --installed
 
 
 
---Task 1. List all organization machines
-SELECT device_id, operating_system 
-FROM machines;
-
---Task 2. Retrieve a list of the machines with OS 2
-SELECT device_id, operating_system 
-FROM machines 
-WHERE operating_system = 'OS 2';
-
---Task 3. List employees in specific departments
-SELECT * 
-FROM employees 
-WHERE department = 'Finance';
-
-Modify the previous query so that it returns employees who are in the 'Sales' department.
-SELECT * 
-FROM employees 
-WHERE department = 'Sales';
-
---Task 4. Identify employee machines
-1.Write a query to identify which employee uses the office in 'South-109'. (The data must be returned from the office column in the employees table.)
-SELECT * 
-FROM employees 
-WHERE office = 'South-109';
-
-2.Modify the query you used in the previous step so that it returns information on all the employees in the 'South' building. Use the LIKE operator with % in this query.
-SELECT * 
-FROM employees 
-WHERE office LIKE 'South%';
